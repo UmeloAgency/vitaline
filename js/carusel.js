270eb2e77e135a5c0f14cd83e49238f59815d906
Alex K <alex.workinfo97@gmail.com>
2024-07-26 22:06:52 +0300

    add banner count

--- js/carusel.js
+++ js/carusel.js
@@ -1,71 +1,10 @@
-// // ДОКТОР
-const docSlider = document.getElementById('slider-doc');
-const docBtnNext = document.getElementById('slider-right-doc');
-const docBtnPrev = document.getElementById('slider-left-doc');
+var $statusActive = $('.slider__count_active');
+var $statusTotal = $('.slider__count_total');
 
-initCarusel(docSlider, docBtnNext, docBtnPrev);
+$('#slider-banner').on('beforeChange', function (a, b, c, nextSlide) {
+  $statusActive.text('0' + ((nextSlide ? nextSlide : 0) + 1));
+});
 
-// ОТЗИВИ
-const reviewsSlider = document.getElementById('slider-reviews');
-const reviewsBtnNext = document.getElementById('slider-right-reviews');
-const reviewsBtnPrev = document.getElementById('slider-left-reviews');
-
-initCarusel(reviewsSlider, reviewsBtnNext, reviewsBtnPrev);
-
-// ПОРТФОЛІО
-const portfolioSlider = document.getElementById('slider-portfolio');
-const portfolioBtnNext = document.getElementById('slider-right-portfolio');
-const portfolioBtnPrev = document.getElementById('slider-left-portfolio');
-
-initCarusel(portfolioSlider, portfolioBtnNext, portfolioBtnPrev);
-
-//  ФУНКЦИЇ
-
-function initCarusel(slider, btnNext, btnPrev) {
-  const sliderItems = Array.from(slider.children);
-
-  sliderItems.forEach(function (slide, index) {
-    slide.dataset.index = index;
-    sliderItems[0].setAttribute('data-active', '');
-  });
-
-  btnNext.onclick = function () {
-    showNextSlide('next', slider, sliderItems);
-  };
-
-  btnPrev.onclick = function () {
-    showNextSlide('prev', slider, sliderItems);
-  };
-}
-
-function showNextSlide(direction, slider, sliderItems) {
-  const styleSlider = getComputedStyle(slider);
-  const styleItem = getComputedStyle(sliderItems[0]);
-  const itemWidth = +sliderItems[0].clientWidth;
-  const itemMarginRight = +styleItem.marginRight.slice(0, -2);
-
-  const currentSlide = slider.querySelector('[data-active]');
-  const currentSlideIndex = +currentSlide.dataset.index;
-  const currentStep = new WebKitCSSMatrix(styleSlider.transform).m41;
-  const step = itemWidth + itemMarginRight;
-  let nextSlideIndex;
-
-  if (direction === 'prev' && currentSlideIndex === 0) return;
-
-  currentSlide.removeAttribute('data-active');
-
-  if (direction === 'next') {
-    nextSlideIndex =
-      currentSlideIndex + 1 === sliderItems.length ? 0 : currentSlideIndex + 1;
-    slider.style.transform = `translateX(-${step * nextSlideIndex}px)`;
-  }
-
-  if (direction === 'prev') {
-    nextSlideIndex =
-      currentSlideIndex === 0 ? sliderItems.length - 1 : currentSlideIndex - 1;
-    slider.style.transform = `translateX(${currentStep + step}px)`;
-  }
-
-  const nextSlide = slider.querySelector(`[data-index="${nextSlideIndex}"]`);
-  nextSlide.setAttribute('data-active', '');
-}
+$('#slider-banner').on('init reinit', function (e, slick) {
+  $statusTotal.text('0' + slick.slideCount);
+});
